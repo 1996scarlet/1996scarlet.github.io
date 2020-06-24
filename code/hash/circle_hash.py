c93266d6726fab822940fbb30b19ad33c83f6b0c
1996scarlet <1996scarlet@gmail.com>
2020-06-24 19:48:40 +0800

    ojbk

--- code/hash/circle_hash.py
+++ code/hash/circle_hash.py
@@ -1,41 +1,65 @@
-from bintrees import FastRBTree
+# from bintrees import FastRBTree
 
 
-def hash_builder(size=0xffffffff):
-    # 0xffffffff = 2**32 - 1
-    def hash_func(x):
-        return size & hash(x)
-    return hash_func
+# def hash_builder(size=0xffffffff):
+#     # 0xffffffff = 2**32 - 1
+#     def hash_func(x):
+#         return size & hash(x)
+#     return hash_func
 
 
-def gen_hashed_ip(hash_func, addresses):
-    for ip in addresses:
-        yield hash_func(ip), ip
+# def gen_hashed_ip(hash_func, addresses):
+#     for ip in addresses:
+#         yield hash_func(ip), ip
 
 
-# 解决偏置
-ip_list = [
-    '10.41.0.101-Real',
-    '10.41.0.101-Fake-A',
-    '10.41.0.101-Fake-B',
-    '10.41.0.102-Real',
-    '10.41.0.102-Fake-A',
-    '10.41.0.102-Fake-B',
-    '10.41.0.103-Real',
-    '10.41.0.103-Fake-A',
-    '10.41.0.103-Fake-B',
-]
+# # 解决偏置
+# ip_list = [
+#     '10.41.0.101-Real',
+#     '10.41.0.101-Fake-A',
+#     '10.41.0.101-Fake-B',
+#     '10.41.0.102-Real',
+#     '10.41.0.102-Fake-A',
+#     '10.41.0.102-Fake-B',
+#     '10.41.0.103-Real',
+#     '10.41.0.103-Fake-A',
+#     '10.41.0.103-Fake-B',
+# ]
 
-my_hash = hash_builder()
+# my_hash = hash_builder()
 
-rbtree = FastRBTree(gen_hashed_ip(my_hash, ip_list))
-# print(rbtree)
+# rbtree = FastRBTree(gen_hashed_ip(my_hash, ip_list))
+# # print(rbtree)
 
-hashed = my_hash('a.jpg')
+# hashed = my_hash('a.jpg')
 
-try:
-    item = next(rbtree[hashed:].items())
-    print(hashed, item)
-except StopIteration:
-    print('闭环')
-    print(hashed, rbtree.min_item())
+# try:
+#     item = next(rbtree[hashed:].items())
+#     print(hashed, item)
+# except StopIteration:
+#     print('闭环')
+#     print(hashed, rbtree.min_item())
+
+import time
+
+def func_timer(func):
+    def insider(*args):
+        st = time.perf_counter()
+        func(*args)
+        print(time.perf_counter() - st)
+    return insider
+
+@func_timer
+def str_add(base, adder, times):
+    for i in range(times):
+        base += adder
+
+@func_timer
+def join_add(base, adder, times):
+    base.join(adder for i in range(times))
+
+N = 10**7
+A = "nginx" * 16
+
+str_add("", A, N)
+join_add("", A, N)
